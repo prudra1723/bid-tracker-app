fbc34cb696b6ead8f8eb6dd21f0e8f0520777b73
Rudra Pandey <22455439@students.latrobe.edu.au>
2026-05-11 09:29:34 +1000

    Updated app publisher metadata

--- bid_tracker/hooks.py
+++ bid_tracker/hooks.py
@@ -1,8 +1,8 @@
 app_name = "bid_tracker"
 app_title = "Bid Tracker"
-app_publisher = "OpenAI"
+app_publisher = "Rudra Pandey"
 app_description = "Custom ERPNext module for pre-contract bid tracking"
-app_email = "support@example.com"
+app_email = "rudra.pandey@bidtracker.com"
 app_license = "MIT"
 
 after_install = "bid_tracker.install.after_install"
