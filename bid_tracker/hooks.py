85d9a8f6f14f5682ca85cb337209743c2ac82d4c
Rudra Pandey <22455439@students.latrobe.edu.au>
2026-05-12 10:15:05 +1000

    Update app email configuration

--- bid_tracker/hooks.py
+++ bid_tracker/hooks.py
@@ -2,7 +2,7 @@ app_name = "bid_tracker"
 app_title = "Bid Tracker"
 app_publisher = "Rudra Pandey"
 app_description = "Custom ERPNext module for pre-contract bid tracking"
-app_email = "rudra.pandey@bidtracker.com"
+app_email = "bidtrackererpnext@gmail.com"
 app_license = "MIT"
 
 after_install = "bid_tracker.install.after_install"
